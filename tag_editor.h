f9dee8c2e8b46ffea903516de1143bd5bdf97d5d
Jeeva <jeevai1806@gmail.com>
2026-09-24 00:59:01 +0000

    In tag header file created enum structure for returning status and also function prototype .mp3 file

---
+++ tag_editor.h
@@ -0,0 +1,16 @@
+#ifndef TAG_EDITOR_H
+#define TAG_EDITOR_H
+
+/* created validation structure */
+typedef enum 
+{
+    SUCCESS,
+    FAILURE
+}Status;
+
+/* Header Inclusion */
+#include<stdio.h>
+
+/* Function prototype */
+Status file_val(FILE *mp3);  // For validating the ID3v2 header of the input file
+#endif
